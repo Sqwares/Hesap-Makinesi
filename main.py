60f022d43e3686791113ffc4dbb76924947884ac
Sqwares <83078854+Sqwares@users.noreply.github.com>
2021-07-09 16:11:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+
+import time
+
+from os import system as s
+
+import colorama
+
+import os 
+
+from colorama import Fore, Back, Style
+
+banner = """       
+██╗    ██╗ █████╗ ██████╗ ███████╗███████╗
+██║    ██║██╔══██╗██╔══██╗██╔════╝██╔════╝
+██║ █╗ ██║███████║██████╔╝█████╗  ███████╗
+██║███╗██║██╔══██║██╔══██╗██╔══╝  ╚════██║
+╚███╔███╔╝██║  ██║██║  ██║███████╗███████║
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+                                          
+>>>>> Coded By Sqwares <<<<<<
+|> Instagram = sqwares_
+"""
+
+print(banner)
+print("Hesap Makinesi Sürüm = 1.0")
+print("")
+print("")
+print("Toplamak İçin= +")
+time.sleep(2)
+print("Çıkarmak İçin = -")
+time.sleep(2)
+print("Çarpmak İçin= *")
+time.sleep(2)
+print("Bölmek İçin= /")
+time.sleep(3)
+print("")
+print("")
+
+
+num1 = float(input("İşleme Alınacak Sayı: "))
+
+op = str(input("İşlemi Girin Örn:(+,-,/,*): "))
+
+num2 = float(input("İşleme Girecek Sayı: "))
+
+if op == '+':
+  print(num1 + num2)
+elif op == '-':
+  print(num1 - num2)
+elif op == '*':
+  print(num1 * num2)
+elif op == '/':
+  print(num1 / num2)
+elif op == '%s':
+   print(num1 % num2)
+elif op == '%':
+   print((num1 / num2) * 100)
+elif op == 'x%':
+   print((num1 / 100) * num2)
+else:
+  print("Geçersiz İşlem")
